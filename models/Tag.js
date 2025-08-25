27853b6b7ab2de4c2239e20e407751ed555a4962
Huy2304 <116505781+Huy2304@users.noreply.github.com>
2025-08-25 11:22:32 +0700

    Custom lại cấu trúc project

--- models/Tag.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const tagSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true, maxlength: 100 }
-  });
-  
-  export default mongoose.model("Tag", tagSchema);
-  
--- routes/interaction.routers.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {toggleInteraction} from "../controllers/Interaction.controllers.js";
-
-const router = express.Router();
-
-router.post("/", toggleInteraction);
-
-export default router;
